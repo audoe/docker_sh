42994a5492983f1980c483ca7ad5527a7c4a3629
whchen <whchen@qq.com>
2016-01-25 20:53:02 +0800

    fix typo

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ apt-get install apt-transport-https
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 bash -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 apt-get update
-apt-get install lxc-docker
+apt-get -y install lxc-docker
 
 sudo docker pull kiwenlau/hadoop-master:0.1.0
 sudo docker pull kiwenlau/hadoop-slave:0.1.0
